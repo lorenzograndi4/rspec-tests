98080543edcd8bc2a9618122decea35731dc61a3
lorenzograndi4 <lorenzo.grandi4@gmail.com>
2017-09-05 18:15:25 +0200

    refactor main method into smaller ones

--- test.rb
+++ test.rb
@@ -1,22 +1,26 @@
 require 'open-uri'
 require 'nokogiri'
 require 'rspec'
-require 'rails-helper'
-# require 'rspec-expectations'
 
-doc = Nokogiri::HTML(open("https://www.marinetraffic.com/en/ais/details/ships/shipid:4199684/mmsi:244670249/vessel:STORMALONG"))
+def import_file
+  Nokogiri::HTML(open("https://www.marinetraffic.com/en/ais/details/ships/shipid:4199684/mmsi:244670249/vessel:STORMALONG"))
+end
 
-def find_lat_lng(doc)
-  string = doc.css('.details_data_link')[0].children.text
-  lat_lng = string.gsub(/[\ °]/,"").split('/')
-  lat_lng.map do |item|
-    item.to_f
-  end
+def extrapolate
+  import_file.css('.details_data_link')[0].children.text
+end
+
+def divide_lat_lng
+  extrapolate.gsub(/[\ °]/,"").split('/')
+end
+
+def save_as_float
+  divide_lat_lng.map { |item| item.to_f }
 end
 
 RSpec.describe "find lat and lng" do
   it "returns lat and lng" do
-    lat, lng = find_lat_lng
+    lat, lng = save_as_float
     expect(lat).to be_between(-90, 90)
     expect(lng).to be_between(-180, 180)
   end
