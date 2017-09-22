2afa99a1ffe94d02c23d27c5a9000f0e8d18bf18
lorenzograndi4 <lorenzo.grandi4@gmail.com>
2017-09-22 15:22:18 +0200

    clean new file

--- exceptions.rb
+++ exceptions.rb
@@ -4,15 +4,3 @@ class RetryException < RuntimeError
     @okToRetry = ok_to_retry
   end
 end
-
-# Somewhere down in the depths of the code, a transient error occurs.
-
-# Higher up the call stack, we handle the exception.
-
-begin
-  stuff = readData(socket)
-  # .. process stuff
-rescue RetryException => detail
-  retry if detail.ok_to_retry
-  raise
-end
